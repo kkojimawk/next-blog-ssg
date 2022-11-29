ac6a14a24372fc0479d43a45c482873420cfa37d
kkojimawk <kkojimawk@gmail.com>
2022-11-29 23:17:11 +0900

    Home作成 getStaticProps

--- pages/index.jsx
+++ pages/index.jsx
@@ -3,7 +3,49 @@ import Image from "next/image";
 import Link from "next/link";
 import Layout from "../components/Layout";
 import styles from "../styles/Home.module.css";
+import utilStyle from "../styles/utils.module.css";
+import { getPostsData } from "../lib/post";
 
-export default function Home() {
-  return <Layout></Layout>;
+//SSG
+export const getStaticProps = async () => {
+  const allPostsData = getPostsData();
+  return {
+    props: {
+      allPostsData,
+    },
+  };
+};
+
+export default function Home({ allPostsData }) {
+  return (
+    <Layout>
+      <section className={utilStyle.headingMd}>
+        <p>私はフロントエンドエンジニアです/好きな言語はJavaScriptです</p>
+      </section>
+
+      <section className={`${utilStyle.headingMd} ${utilStyle.padding1px}`}>
+        <h1 className="">🗒エンジニアのブログ</h1>
+        <div className={styles.grid}>
+          {allPostsData.map(({ id, title, date, thumbnail }) => (
+            <article key={id}>
+              <Link href={`/posts/${id}`}>
+                <Image
+                  className={styles.thumbnailImage}
+                  src={thumbnail}
+                  alt=""
+                  width={950}
+                  height={400}
+                />
+              </Link>
+              <Link href="/" className="time">
+                {title}
+              </Link>
+              <br />
+              <small>{date}</small>
+            </article>
+          ))}
+        </div>
+      </section>
+    </Layout>
+  );
 }
