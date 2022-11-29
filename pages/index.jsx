c590c22c0901aca02fbfca37a61e297646b488fc
kkojimawk <kkojimawk@gmail.com>
2022-11-29 19:40:38 +0900

    index.jsx作成

---
+++ pages/index.jsx
@@ -0,0 +1,9 @@
+import Head from "next/head";
+import Image from "next/image";
+import Link from "next/link";
+import Layout from "../components/Layout";
+import styles from "../styles/Home.module.css";
+
+export default function Home() {
+  return <Layout></Layout>;
+}
