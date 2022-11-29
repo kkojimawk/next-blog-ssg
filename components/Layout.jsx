8afe2275224bfd0ddc8e37750b1d1bb66143e4fa
kkojimawk <kkojimawk@gmail.com>
2022-11-29 23:32:37 +0900

    ホームに戻るボタン

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -1,19 +1,46 @@
 import Head from "next/head";
 import styles from "./layout.module.css";
 import utilStyles from "../styles/utils.module.css";
+import Image from "next/image";
+import Link from "next/link";
 
 const name = "Kojima";
-const Layout = ({ children }) => {
+const Layout = ({ children, home }) => {
   return (
     <div className={styles.container}>
       <Head>
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <header className={styles.header}>
-        <img className={utilStyles.borderCircle} src="/images/profile.png" />
+        {home ? (
+          <>
+            <Image
+              src="/images/profile.png"
+              alt=""
+              className={`${utilStyles.borderCircle} ${styles.headerHomeImage}`}
+              width={144}
+              height={144}
+            />
+          </>
+        ) : (
+          <>
+            <Image
+              className={utilStyles.borderCircle}
+              src="/images/profile.png"
+              alt=""
+              width={108}
+              height={108}
+            />
+          </>
+        )}
         <h1 className={utilStyles.heading2Xl}>{name}</h1>
       </header>
       <main>{children}</main>
+      {!home && (
+        <div className={styles.backToHome}>
+          <Link href="/">← ホームに戻る</Link>
+        </div>
+      )}
     </div>
   );
 };
